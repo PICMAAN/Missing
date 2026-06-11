101867d22d5c52eb07b8d52153c72ec6bbc0f3c6
PICMAAN <luis.maldonado@uniat.edu.mx>
2026-06-11 16:28:31 -0600

    .

--- Assets/scripts/MovimientoMadre.cs
+++ Assets/scripts/MovimientoMadre.cs
@@ -1,40 +1,41 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class MovimientoMadre : MonoBehaviour
 {
     private Rigidbody2D rig;
     private Animator anim;
-    private SpriteRender spritePersonaje;
+    private SpriteRenderer spritePersonaje;
 
+    public float velocidad = 5f;
+
+    private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-        anim = GetComponetChildren<Animator>();
-        spritePersonaje = GetComponetChildren<SpriteRenderer>();
-}
+        anim = GetComponentInChildren<Animator>();
+        spritePersonaje = GetComponentInChildren<SpriteRenderer>();
+    }
 
     private void FixedUpdate()
     {
-    Movimiento();
+        Movimiento();
     }
 
     private void Movimiento()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical")
+        float vertical = Input.GetAxis("Vertical");
 
-        rig.velocity = new Vector2(horizontal, vertical) * velocidad;
-        anim.SetFloat("camina", Mathf.Abs(rig.velocity.magnitude));
+        rig.linearVelocity = new Vector2(horizontal, vertical) * velocidad;
 
-    if (horizontal > 0)
-    {
-        spritePersonaje.flipX = false
-    }
-    else if (vertical > 0) 
-    {
-        spritePersonaje.flipX = true
+        anim.SetFloat("camina", rig.linearVelocity.magnitude);
+
+        if (horizontal > 0)
+        {
+            spritePersonaje.flipX = false;
+        }
+        else if (horizontal < 0)
+        {
+            spritePersonaje.flipX = true;
+        }
     }
 }
