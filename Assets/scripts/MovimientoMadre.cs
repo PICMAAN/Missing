b764da5b42fde149f5f9f73d55ecc21d1cd4889c
PICMAAN <luis.maldonado@uniat.edu.mx>
2026-06-15 02:58:23 -0600

    continuacion

--- Assets/scripts/MovimientoMadre.cs
+++ Assets/scripts/MovimientoMadre.cs
@@ -5,14 +5,13 @@ public class MovimientoMadre : MonoBehaviour
     private Rigidbody2D rig;
     private Animator anim;
     private SpriteRenderer spritePersonaje;
-
     public float velocidad = 5f;
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-        anim = GetComponentInChildren<Animator>();
-        spritePersonaje = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        spritePersonaje = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -26,16 +25,11 @@ public class MovimientoMadre : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         rig.linearVelocity = new Vector2(horizontal, vertical) * velocidad;
-
         anim.SetFloat("camina", rig.linearVelocity.magnitude);
 
         if (horizontal > 0)
-        {
             spritePersonaje.flipX = false;
-        }
         else if (horizontal < 0)
-        {
             spritePersonaje.flipX = true;
-        }
     }
 }
